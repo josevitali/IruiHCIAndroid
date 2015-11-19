18af9ac9e20ada52cd4ad19466b0dc0dbbe3b5bc
natinavas <nnavas@itba.edu.ar>
2015-11-19 01:10:44 -0300

    busqeuda cada vez mas lindo`
    y saque de ordenar por ese pobrecito de mierda

--- Irui2/app/src/main/java/com/example/usuario/irui/Busqueda.java
+++ Irui2/app/src/main/java/com/example/usuario/irui/Busqueda.java
@@ -6,13 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.LayoutInflater;
+import android.content.Context;
 
-public class Busqueda extends AppCompatActivity {
+
+public class Busqueda extends Base {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_busqueda);
+//        setContentView(R.layout.activity_busqueda);
+
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_busqueda, null, false);
+        drawer.addView(contentView, 0);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
