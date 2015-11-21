a0aef091f1aa79879e307f0ecbf6e949f131271a
natinavas <nnavas@itba.edu.ar>
2015-11-21 20:11:07 -0300

    cambia el iconito del estado de la orden

--- Irui2/app/src/main/java/com/example/usuario/irui/adapters/OrderArrayAdapter.java
+++ Irui2/app/src/main/java/com/example/usuario/irui/adapters/OrderArrayAdapter.java
@@ -2,6 +2,7 @@ package com.example.usuario.irui.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.Image;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -42,19 +43,31 @@ public class OrderArrayAdapter extends ArrayAdapter<Order>{
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_order, parent, false);
         }
 
-        TextView nameTextView = (TextView)convertView.findViewById(R.id.addressName);
+        TextView nameTextView = (TextView) convertView.findViewById(R.id.addressName);
         nameTextView.setText(order.getAddress() == null ? "-" : order.getAddress().getName());
 
-        TextView brandTextView = (TextView)convertView.findViewById(R.id.orderId);
+        TextView brandTextView = (TextView) convertView.findViewById(R.id.orderId);
         brandTextView.setText("Id: #" + order.getId().toString());
 
 
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.statusIcon);
+
+
         OrderStatus status = order.getStatus();
+        if (status == OrderStatus.CREATED) {
+            imageView.setImageResource(R.drawable.created);
 
 
-        if(order.getStatus() == OrderStatus.CREATED){
-            
+        } else if (status == OrderStatus.DELIVERED){
+
+            imageView.setImageResource(R.drawable.shipped);
+
+        }else if(status == OrderStatus.SHIPPED){
+            imageView.setImageResource(R.drawable.process);
+        }else{
+            imageView.setImageResource(R.drawable.confirmed);
         }
+
 
 
         return convertView;
