8a70d5ecd71a1e6c22ab28c61d6065f807f94786
natinavas <nnavas@itba.edu.ar>
2015-11-21 22:37:06 -0300

    orders

--- Irui2/app/src/main/java/com/example/usuario/irui/Orders.java
+++ Irui2/app/src/main/java/com/example/usuario/irui/Orders.java
@@ -1,6 +1,7 @@
 package com.example.usuario.irui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -104,6 +105,17 @@ public class Orders extends Base {
             Toast.makeText(getApplicationContext(),"exception",
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+
+
+    public void openOrder(View view){
+        Toast.makeText(getApplicationContext(),"no exploto pero entro",
+                Toast.LENGTH_LONG).show();
+
+
+//        Intent intent = new Intent(this, Order.class);
+//        this.startActivity(intent);
     }
 
 }
